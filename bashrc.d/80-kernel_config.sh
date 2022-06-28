d20e9ff8f3d5b8173a0e37bbd6ffb52b82318d45
Lucius Martius <lucius-martius@dorsai.eu>
2022-06-28 17:40:36 +0200

    Fix kernel configuration script shebang

--- bashrc.d/80-kernel_config.sh
+++ bashrc.d/80-kernel_config.sh
@@ -4,7 +4,7 @@ Kernel_Config_Configure(){
   einfo "Adding a config script to work directory"
   local scriptfile="${WORKDIR}/make.sh"
   echo "#!/usr/bin/env bash" > "${scriptfile}"
-  echo "make O=${WORKDIR}/build -C ${S} \${1}" > "${scriptfile}"
+  echo "make O=${WORKDIR}/build -C ${S} \${1}" >> "${scriptfile}"
   chmod +x "${scriptfile}" || die
 }
 
