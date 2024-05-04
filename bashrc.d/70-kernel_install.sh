79edf0c714ace3334f27da31f0528245b0da4509
Lucius Martius <lucius-martius@dorsai.eu>
2024-05-04 19:11:20 +0200

    Fix install phase and remove image from /usr/src tree

--- bashrc.d/70-kernel_install.sh
+++ bashrc.d/70-kernel_install.sh
@@ -4,14 +4,17 @@ Kernel_Install_Install() {
   local kern_arch="${ARCH/amd64/x86_64}"
   local kern_name="${PN%-kernel}"
   local kern_ver="${PV}"
+  # shellcheck disable=SC2155
+  local kern_path="${WORKDIR}/build/$(dist-kernel_get_image_path)"
   local kern_file="vmlinuz-${kern_ver}-${kern_name}-${kern_arch}"
 
   mount-boot_pkg_preinst
 
   exeinto "/boot"
-  newexe "${WORKDIR}/build/$(dist-kernel_get_image_path)" "${kern_file}"
+  newexe "${kern_path}" "${kern_file}"
+  rm "${kern_path}"
 }
 
 if Kernel_IsOneOf gentoo-kernel vanilla-kernel; then
-  BashrcdPhase preinst Kernel_Install_Install
+  BashrcdPhase install Kernel_Install_Install
 fi
